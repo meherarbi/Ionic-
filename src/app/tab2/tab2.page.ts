e31a4ce08ab6d1d724deb94fbea944effa36ff2c
meherarbi <meher.arbi@gmail.com>
2021-12-15 10:31:30 +0100

    first commit

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { favoriteList, Tab1Page } from '../tab1/tab1.page';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+  favorites: {name: string, phone: number, img: string}[]  ;
+  
+  constructor() {
+    this.favorites= favoriteList;
+    console.log(this.favorites);
+    console.log(this.favorites.length);
+  }
+
+} 
+
